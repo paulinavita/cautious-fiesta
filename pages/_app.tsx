26e36e0dbda120b95cedc0f523032549bd836fb0
Paulina Davita <paulina.davita@dana.id>
2022-07-02 12:41:57 +0700

    fix: missing path

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,7 +15,7 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
-import createEmotionCache from '../utility/createEmotionCache';
+import createEmotionCache from '../utils/createEmotionCache';
 import getTheme from 'themes';
 import '../styles/globals.css';
 interface PropsPage extends AppProps {
